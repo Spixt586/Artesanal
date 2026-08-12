3afd81fdb4f981d29cad8cb15b38418e9dedcb7d
Spixt586 <141528122+Spixt586@users.noreply.github.com>
2026-08-11 20:33:39 -0500

    feat: validaciones unitarias

--- src/com/krakedev/artesanal/Maquina.java
+++ src/com/krakedev/artesanal/Maquina.java
@@ -77,13 +77,13 @@ public class Maquina {
 	}
 	
 	public void llenarMaquina() {
-		this.cantidadActual = this.capacidadMaxima - 200;
+		this.cantidadActual = this.capacidadMaxima - 100;
 	}
 	
 	public boolean recargarCerveza(double cantidad) {
 		
 		double limitePermitido;
-		limitePermitido = capacidadMaxima - 200;
+		limitePermitido = capacidadMaxima - 100;
 		
 		if(cantidadActual + cantidad <= limitePermitido) {
 			
--- src/module-info.java
+++ src/module-info.java
@@ -5,6 +5,5 @@
  * 
  */
 module Artesana {
-	requires jdk.incubator.vector;
 	requires org.junit.jupiter.api;
 }
